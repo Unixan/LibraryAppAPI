3ba325a6380382cd60073dcd13149152a814a081
Ståle Johansen <staale_80@hotmail.com>
2023-06-15 19:50:16 +0200

    WIP

--- LibraryAppAPI/Program.cs
+++ LibraryAppAPI/Program.cs
@@ -1,7 +1,7 @@
 
-using System.Data.SqlClient;
 using Dapper;
 using LibraryAppAPI.Model;
+using System.Data.SqlClient;
 
 namespace LibraryAppAPI
 {
@@ -53,67 +53,75 @@ namespace LibraryAppAPI
                 var users = await conn.QueryAsync<User>(userSql);
                 return users;
             });
-                //.WithName("LibraryUsers")
-                //.WithOpenApi();
-                app.MapGet("/libraryBooks", async () =>
-                {
-                    var conn = new SqlConnection(connStr);
-                    const string sql = "SELECT * FROM [Book]";
-                    var books = await conn.QueryAsync<Book>(sql);
-                    return books;
-                });
-                //.WithName("LibraryBooks")
-                //.WithOpenApi();
-                app.MapGet("/libraryUser/{id}", async (Guid id) =>
-                {
-                    var conn = new SqlConnection(connStr);
-                    const string sql = "SELECT * FROM [User] WHERE UserID = @UserId";
-                    var user = await conn.QueryAsync(sql, new { UserID = id });
-                    return user;
-                });
-                app.MapPost("/libraryUser", async (User user) =>
-                {
-                    var conn = new SqlConnection(connStr);
-                    const string sql = "INSERT [User] (FirstName, LastName, Address, UserID, LoanCard) VALUES (@FirstName, @LastName, @Address, @UserID, 'Ingen')";
-                    var newUserInsert = await conn.ExecuteAsync(sql, user);
-                    return newUserInsert;
-                });
-                app.MapDelete("/libraryUser{id}", async (Guid id) =>
-                {
-                    var conn = new SqlConnection(connStr);
-                    const string sql = "DELETE FROM [User] WHERE UserID = @UserId";
-                    var userToDelete = await conn.ExecuteAsync(sql, new { UserID = id });
-                    return userToDelete;
-                } );
-               
-                //.WithName("LibraryUser")
-                //.WithOpenApi();
-                app.MapGet("/libraryBook/{id}", async (Guid id) =>
-                {
-                    var conn = new SqlConnection(connStr);
-                    const string sql = "SELECT * FROM [Book] WHERE BookId = @BookId";
-                    var book = await conn.QueryAsync(sql, new { BookId = id });
-                    return book;
-                });
-                app.MapPost("/libraryBook", async (Book book) =>
-                {
-                    var conn = new SqlConnection(connStr);
-                    const string sql = "INSERT [Book] (BookId, Title, Author, Genre, Description, LoanedToId, LoanedOnDate) VALUES (@BookId, @Title, @Author, @Genre, @Description, NULL, NULL)";
-                    var newBookInsert = await conn.ExecuteAsync(sql, book);
-                    return newBookInsert;
-                });
-                app.MapDelete("/libraryBook{id}", async (Guid id) =>
-                {
-                    var conn = new SqlConnection(connStr);
-                    const string sql = "DELETE FROM [Book] WHERE BookID = @BookId";
-                    var bookToDelete = await conn.ExecuteAsync(sql, new { BookId = id });
-                    return bookToDelete;
-                });
-                //.WithName("LibraryBook")
-                //.WithOpenApi();
+           app.MapGet("/libraryUser/{id}", async (Guid id) =>
+            {
+                var conn = new SqlConnection(connStr);
+                const string sql = "SELECT * FROM [User] WHERE UserID = @UserId";
+                var user = await conn.QueryAsync(sql, new { UserID = id });
+                return user;
+            });
+            app.MapPut("/libraryUser/{user.UserID}", async (User user) =>
+            {
+                var conn = new SqlConnection(connStr);
+                const string sql = "UPDATE [User] SET LoanCard = @LoanCard WHERE UserID = @UserID";
+                var changeUserLoanCard = await conn.ExecuteAsync(sql, user);
+                return changeUserLoanCard;
+
+            });
+            app.MapPost("/libraryUser", async (User user) =>
+            {
+                var conn = new SqlConnection(connStr);
+                const string sql = "INSERT [User] (FirstName, LastName, Address, UserID, LoanCard) VALUES (@FirstName, @LastName, @Address, @UserID, 'Ingen')";
+                var newUserInsert = await conn.ExecuteAsync(sql, user);
+                return newUserInsert;
+            });
+            app.MapDelete("/libraryUser/{id}", async (Guid id) =>
+            {
+                var conn = new SqlConnection(connStr);
+                const string sql = "DELETE FROM [User] WHERE UserID = @UserId";
+                var userToDelete = await conn.ExecuteAsync(sql, new { UserID = id });
+                return userToDelete;
+            });
+            app.MapGet("/libraryBooks", async () =>
+            {
+                var conn = new SqlConnection(connStr);
+                const string sql = "SELECT * FROM [Book]";
+                var books = await conn.QueryAsync<Book>(sql);
+                return books;
+            });
+            app.MapGet("/libraryBook/{id}", async (Guid id) =>
+            {
+                var conn = new SqlConnection(connStr);
+                const string sql = "SELECT * FROM [Book] WHERE BookId = @BookId";
+                var book = await conn.QueryAsync(sql, new { BookId = id });
+                return book;
+            });
+            app.MapPut("/libraryBook/{book.BookId}", async (Book book) =>
+            {
+                var conn = new SqlConnection(connStr);
+                const string sql = "UPDATE [Book] SET LoanedToID = @LoanedToId, LoanedOnDate = @LoanedOnDate WHERE BookId = @BookId";
+                var bookLoanedToStatus = await conn.ExecuteAsync(sql, book);
+                return bookLoanedToStatus;
+            });
+            app.MapPost("/libraryBook", async (Book book) =>
+            {
+                var conn = new SqlConnection(connStr);
+                const string sql = "INSERT [Book] (BookId, Title, Author, Genre, Description, LoanedToId, LoanedOnDate) VALUES (@BookId, @Title, @Author, @Genre, @Description, NULL, NULL)";
+                var newBookInsert = await conn.ExecuteAsync(sql, book);
+                return newBookInsert;
+            });
+            app.MapDelete("/libraryBook/{id}", async (Guid id) =>
+            {
+                var conn = new SqlConnection(connStr);
+                const string sql = "DELETE FROM [Book] WHERE BookID = @BookId";
+                var bookToDelete = await conn.ExecuteAsync(sql, new { BookId = id });
+                return bookToDelete;
+            });
+            //.WithName("LibraryBook")
+            //.WithOpenApi();
 
             app.Run();
         }
-        
+
     }
 }
